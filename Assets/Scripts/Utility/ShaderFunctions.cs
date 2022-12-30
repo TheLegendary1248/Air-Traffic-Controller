601f3625dce197c12be25277b6ad9d31fc1cf16d
TheLegendary1248 <1248alex@gmail.com>
2022-12-29 23:24:27 -0500

    Almost there, hopefully it doesn't break

--- Assets/Scripts/Utility/ShaderFunctions.cs
+++ Assets/Scripts/Utility/ShaderFunctions.cs
@@ -19,16 +19,16 @@ public static class ShaderFunctions
         Vector4 ixy0 = wglnoise_permute(ixy + iz0);
         Vector4 ixy1 = wglnoise_permute(ixy + iz1);
 
-        Vector4 gx0 = lerp(-Vector4.one, Vector4.one, frac(floor(ixy0 / 7) / 7));
-        Vector4 gy0 = lerp(-Vector4.one, Vector4.one, frac(floor(ixy0 % 7) / 7));
+        Vector4 gx0 = lerp4(-Vector4.one, Vector4.one, frac(floor(ixy0 / 7) / 7));
+        Vector4 gy0 = lerp4(-Vector4.one, Vector4.one, frac(floor(ixy0 % 7) / 7));
         Vector4 gz0 = Vector4.one - gx0.Abs() - gy0.Abs();
 
         bool4 zn0 = gz0 < -0.01f;
         gx0 += zn0 * (gx0 < -0.01f ? 1 : -1);
         gy0 += zn0 * (gy0 < -0.01f ? 1 : -1);
 
-        Vector4 gx1 = lerp(-Vector4.one, Vector4.one, frac(floor(ixy1 / 7) / 7));
-        Vector4 gy1 = lerp(-Vector4.one, Vector4.one, frac(floor(ixy1 % 7) / 7));
+        Vector4 gx1 = lerp4(-Vector4.one, Vector4.one, frac(floor(ixy1 / 7) / 7));
+        Vector4 gy1 = lerp4(-Vector4.one, Vector4.one, frac(floor(ixy1 % 7) / 7));
         Vector4 gz1 = Vector4.one - gx1.Abs() - gy1.Abs();
 
         bool4 zn1 = gz1 < -0.01f;
@@ -80,7 +80,7 @@ public static class ShaderFunctions
 
     static Vector3 frac(Vector3 val)
     {
-        return new Vector3(val.x % 1, val.y % 1, val.z % 1);
+        return new Vector3(val.x % 1f, val.y % 1f, val.z % 1f);
     }
     #region VectorFloor
     static Vector2 floor(Vector2 val)
@@ -108,42 +108,42 @@ public static class ShaderFunctions
 
     static Vector3 wglnoise_mod(Vector3 x, Vector3 y)
     {
-        return x - y * floor(x / y);
+        return x - mul3(y, floor(div3(x, y)));
     }
 
     static Vector4 wglnoise_mod(Vector4 x, Vector4 y)
     {
-        return x - y * floor(x / y);
+        return x - mul4(y, floor(div4(x, y)));
     }
 
     static Vector2 wglnoise_fade(Vector2 t)
     {
-        return t * t * t * (t * (t * 6 - 15) + 10);
+        return t * t * t * (t * (t * 6f - vec2(15f)) + vec2(10f));
     }
 
     static Vector3 wglnoise_fade(Vector3 t)
     {
-        return t * t * t * (t * (t * 6 - 15) + 10);
+        return mul3(mul3(mul3(t, t), t), mul3(t, t * 6f - vec3(15f)) + vec3(10f));
     }
 
     static float wglnoise_mod289(float x)
     {
-        return x - floor(x / 289) * 289;
+        return x - Mathf.Floor(x / 289f) * 289f;
     }
 
     static Vector2 wglnoise_mod289(Vector2 x)
     {
-        return x - floor(x / 289) * 289;
+        return x - floor(x / 289f) * 289f;
     }
 
     static Vector3 wglnoise_mod289(Vector3 x)
     {
-        return x - floor(x / 289) * 289;
+        return x - floor(x / 289f) * 289f;
     }
 
     static Vector4 wglnoise_mod289(Vector4 x)
     {
-        return x - floor(x / 289) * 289;
+        return x - floor(x / 289f) * 289f;
     }
 
     static Vector3 wglnoise_permute(Vector3 x)
@@ -159,12 +159,23 @@ public static class ShaderFunctions
         m.Scale(x);
         return wglnoise_mod289(m);
     }
-    public static Vector2 lerp(Vector2 a, Vector2 b, Vector2 c)
+    public static Vector2 lerp2(Vector2 a, Vector2 b, Vector2 c)
         => new Vector2(Mathf.Lerp(a.x, b.x, c.x), Mathf.Lerp(a.y, b.y, c.y));
-    public static Vector3 lerp(Vector3 a, Vector3 b, Vector3 c)
+    public static Vector3 lerp3(Vector3 a, Vector3 b, Vector3 c)
         => new Vector3(Mathf.Lerp(a.x, b.x, c.x), Mathf.Lerp(a.y, b.y, c.y), Mathf.Lerp(a.z, b.z, c.z));
-    public static Vector4 lerp(Vector4 a, Vector4 b, Vector4 c) 
-        => new Vector4(Mathf.Lerp(a.x,b.x,c.x), Mathf.Lerp(a.y, b.y, c.y), Mathf.Lerp(a.z, b.z, c.z), Mathf.Lerp(a.w, b.w, c.w));
+    public static Vector4 lerp4(Vector4 a, Vector4 b, Vector4 c) 
+        => new Vector4(Mathf.Lerp(a.x, b.x, c.x), Mathf.Lerp(a.y, b.y, c.y), Mathf.Lerp(a.z, b.z, c.z), Mathf.Lerp(a.w, b.w, c.w));
+    public static Vector3 div3(Vector3 a, Vector3 b)
+        => new Vector4(a.x / b.x, a.y / b.y, a.z / b.z);
+    public static Vector4 div4(Vector4 a, Vector4 b)
+        => new Vector4(a.x / b.x, a.y / b.y, a.z / b.z, a.w / b.w);
+    public static Vector3 mul3(Vector3 a, Vector3 b)
+        => new Vector4(a.x * b.x, a.y * b.y, a.z * b.z);
+    public static Vector4 mul4(Vector4 a, Vector4 b)
+        => new Vector4(a.x * b.x, a.y * b.y, a.z * b.z, a.w * b.w);
+    public static Vector2 vec2(float a) => new Vector2(a, a);
+    public static Vector3 vec3(float a) => new Vector3(a, a, a);
+    public static Vector4 vec4(float a) => new Vector4(a, a, a, a);
 }
 public static class VectorExt
 {
@@ -189,6 +200,9 @@ public struct bool4
         this.z = z;
         this.w = w;
     }
-    public static bool4 operator < (bool4 a, float b) 
+    /*
+    public static bool4 operator < (Vector4 a, float b) 
         => new bool4(a.x < b, a.y < b, a.z < b, a.w < b);
+    public static bool4 operator >(Vector4 a, float b)
+        => new bool4(a.x > b, a.y > b, a.z > b, a.w > b); */
 }
