6cab8f4322a8c8ae23391c59057f32d6c60aef50
TheLegendary1248 <1248alex@gmail.com>
2022-12-30 01:26:30 -0500

    Gradient to Texture

--- Assets/Terrain2DScript.cs
+++ Assets/Terrain2DScript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Terrain2DScript : MonoBehaviour
 {
+    public Gradient gradient;
+    public SpriteRenderer spriteRend;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRend.sprite = Sprite.Create(GradientToTex(gradient,256), Rect.MinMaxRect(0,0,1,1), Vector2.zero);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    /// <summary>
+    /// Creates a one pixel high texture gradient for the shader
+    /// </summary>
+    /// <param name="col"></param>
+    /// <param name="resolution"></param>
+    /// <returns></returns>
+    public static Texture2D GradientToTex(Gradient col, int resolution)
+    {
+        Texture2D tex = new Texture2D(resolution, 1);
+        for (int pix = 0; pix < resolution; pix++) tex.SetPixel(pix, 0, col.Evaluate((float)pix / resolution));
+        tex.Apply();
+        return tex;
     }
 }
