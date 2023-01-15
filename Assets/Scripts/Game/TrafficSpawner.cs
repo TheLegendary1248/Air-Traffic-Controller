20fdb002d4d59c0472c1468e726a7e67f8f81768
TheLegendary1248 <1248alex@gmail.com>
2023-01-15 07:21:51 -0500

    Half-functional function for getting point on Rect edge
    Largely untested, and it's not even centered on the Rect

--- Assets/Scripts/Game/TrafficSpawner.cs
+++ Assets/Scripts/Game/TrafficSpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TrafficSpawner : MonoBehaviour
 {
     public GameObject plane;
+    public float spawnTime;
     public Coroutine timer;
 
     // Start is called before the first frame update
@@ -16,13 +17,25 @@ public class TrafficSpawner : MonoBehaviour
     {
         SpawnPlane();
         yield return new WaitForSeconds(time);
-        timer = StartCoroutine(Wave(5f));
+        timer = StartCoroutine(Wave(spawnTime));
     }
     void SpawnPlane()
     {
-        Vector2 v = Random.insideUnitCircle;
-        v.Normalize(); v *= 100;
+        Vector2 v = World.Main.AlignToWorld(GetSpwnPt());
         float angle = Vector2.SignedAngle(Vector2.down, v);
         GameObject gb = Instantiate(plane, new Vector3(v.x,70f + v.y,-3f), Quaternion.Euler(0,0,angle));
+    }
+    Vector2 GetSpwnPt()
+    {
+        Rect border = World.Main.worldBorder;
+        int axis = Random.value > 0.5f ? 1 : 0 ; //X or Y side
+        int side = Random.value > 0.5f ? 1 : -1; //This or opposing side
+        float mul = Random.value * 2f - 1f; //Interpolation
+        Vector2 l = Vector2.zero;
+        float w = border.width / 2f;
+        float h = border.height / 2f;
+        l[axis] = side * (axis == 1 ? w : h);
+        l[(axis - 1) * -1] = mul * (axis == 1 ? h : w);
+        return l;
     }
 }
